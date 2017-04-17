a9fff884cb76a5333e14cbb624b6c8a32c1a8bce
daveyx <d.rehle@gmail.com>
2017-04-17 04:17:20 +0200

    removed play code

--- dev/js/components/pomodoro.js
+++ dev/js/components/pomodoro.js
@@ -8,28 +8,6 @@ import SessionLengthContainer from '../containers/sessionLengthContainer';
 export default class Pomodoro extends Component {
   constructor(props) {
     super();
-    this.state = {
-      currentCount: 10
-    };
-    this.timer = this.timer.bind(this)
-  }
-
-  componentDidMount() {
-    var intervalId = setInterval(this.timer, 1000);
-    this.setState({intervalId: intervalId});
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId);
-  }
-
-  timer() {
-    let newCount = this.state.currentCount -1;
-    this.setState({ currentCount: newCount });
-    if (newCount <= 0) {
-      clearInterval(this.state.intervalId);
-    }
-    //console.log(this.state.currentCount);
   }
 
   render() {
